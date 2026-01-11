bce1e8a8070f6b7d887a2121ad5e930079fe0868
Jan Werder <jan@werder.space>
2026-01-11 14:03:50 +0100

    Update Logic

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,16 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/buddhi1980/mandelbulber2/releases/download/2.27/Mandelbulber2-v2.27.0.0-Setup.exe'
+$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/buddhi1980/mandelbulber2/releases/download/2.34/Mandelbulber2-v2.34.2-Setup.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
-  softwareName  = 'Mandelbulber2*'
-  checksum      = ''
-  checksumType  = 'sha256' 
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'Mandelbulber2*'
+  checksum       = '0929eab9126f759dc8a9a7c64c579e8cd3bac92bb923e5c93d516dc97996f3a2'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
+Install-ChocolateyPackage @packageArgs
--- update.ps1
+++ update.ps1
@@ -1,27 +1,26 @@
 import-module au
 
-$releases = 'https://github.com/buddhi1980/mandelbulber2/releases/'
-
 function global:au_SearchReplace {
     @{
         'tools\chocolateyinstall.ps1' = @{
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
-        'chocolatey-mandelbulber2.nuspec'  = @{
-            "<id>.*</id>" = "<id>mandelbulber2</id>"
+        'mandelbulber2.nuspec' = @{
+            "(<version>).*?(</version>)" = "`$1$($Latest.Version)`$2"
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $release = Invoke-RestMethod -Uri 'https://api.github.com/repos/buddhi1980/mandelbulber2/releases/latest'
+    $asset = $release.assets | Where-Object { $_.name -match 'Setup\.exe$' } | Select-Object -First 1
 
-    $re = 'Mandelbulber2-v(?<version>\d+\.\d+\.\d+).*-Setup.exe'
-    $url = "https://github.com$($download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href)"
-    $version = ($url -split "/" | Select-Object -Index 7)
+    $url = $asset.browser_download_url
+    $version = $release.tag_name
+    $checksum = Get-RemoteChecksum $url
 
-    return @{ URL32 = $url; Version = $version; ChecksumType32 = 'sha256'; }
+    return @{ URL32 = $url; Version = $version; Checksum32 = $checksum; ChecksumType32 = 'sha256' }
 }
 
 update
